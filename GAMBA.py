dbc8362d5c5418be53d026d3f456a26a6cb11c4c
Sebik9000 <159123960+Sebik9000@users.noreply.github.com>
2024-03-26 09:13:34 +0100

    COMMIT

---
+++ GAMBA.py
@@ -0,0 +1,22 @@
+print("Vítejte v kalkulačce, pokud chcete program ukončit napište 'konec' do operace.")
+while True:
+  cislo1 = float(input("Zadej první číslo:" ))
+  cislo2 = float(input("Zadej druhé číslo:" ))
+  operace = input("Zadej operaci '+, -, *, /, **'")
+  if operace == "+":
+    print(cislo1 + cislo2)
+  elif operace == "-":
+    print(cislo1 - cislo2)
+  elif operace == "*":
+    print(cislo1 * cislo2)
+  elif (cislo1 == 0 or cislo2 == 0) and operace == "/":
+    print("Nulou nelze dělit")
+  elif operace == "/":
+    print(cislo1 / cislo2)
+  elif operace == "**":
+    print(cislo1 ** cislo2)
+  elif operace == "konec":
+    print("Děkujeme za užití kalkulačky")
+    break
+  else:
+    print("Co tam sakra píšeš")
